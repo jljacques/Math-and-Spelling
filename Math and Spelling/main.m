b7a367e2e62579684037272e147f88e1b19a6418
Jeremy Jacques <jacquesjeremy@Jeremy-Jacques.local>
2013-05-22 20:39:43 -0400

    Initial Commit

---
+++ Math and Spelling/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Math and Spelling
+//
+//  Created by Jeremy Jacques on 2013-05-22.
+//  Copyright Appstract Art 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
